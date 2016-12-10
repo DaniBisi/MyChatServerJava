98da97b9554affc7cdf2f4d4b2347f147d493f00
daniele <daniele@daniele-Desktop>
2016-12-10 15:47:46 +0100

    Sonar scanner added to pom

--- MyChatServerJava/src/unifi/inf/rc/DanieleBisignano/MyChatServer.java
+++ MyChatServerJava/src/unifi/inf/rc/DanieleBisignano/MyChatServer.java
@@ -94,7 +94,7 @@ public class MyChatServer implements Runnable {
 		return errorFound;
 	}
 
-	@Override
+	//@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (true) {
@@ -186,7 +186,7 @@ public class MyChatServer implements Runnable {
 				MyChatServer.Register.remove(user);
 				MyChatServer.Register.put(user, a);
 			}
-			else{ // controllo se è il suo.
+			else{ // controllo se ï¿½ il suo.
 				Pair<String, Integer> b = MyChatServer.Register.get(user);
 				if(b.equals(a)){// l'ho trovato ed era il suo. quindi aggiorno
 					found = false;
